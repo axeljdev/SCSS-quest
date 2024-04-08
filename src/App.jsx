63eca865f3f6aac999e11f62115860051e79a8b0
AXEL JANIAK <axeljpro@gmail.com>
2024-04-08 10:11:09 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <section className="card">
+        <img src="https://static.fnac-static.com/multimedia/Images/FR/NR/e3/36/b3/11744995/1540-1/tsp20200617101137/Adios-Bahamas.jpg" alt="nepal"></img>
+        <section className='description'>
+        <h1>Nepal - Adios Bahamas</h1>
+        <h2>10 janvier 2020 - Rap</h2>
+        <section className='star'>
+        <img src='img/star_1828970.png'></img>
+        <img src='img/star_1828970.png'></img>
+        <img src='img/star_1828970.png'></img>
+        <img src='img/star_1828970.png'></img>
+        <img src='img/star_1828970.png'></img>
+        </section>
+        </section>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+function Card() {
+    <section className="card">
+        <img src="https://static.fnac-static.com/multimedia/Images/FR/NR/e3/36/b3/11744995/1540-1/tsp20200617101137/Adios-Bahamas.jpg" alt="nepal"></img>
+        <h1>Nepal - Adios bahamas</h1>
+        <p>RIP</p>
+    </section>
+}
+
+export default Card
