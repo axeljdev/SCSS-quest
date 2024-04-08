65d1a651c3bce7354ca2e09668798e12d4cbe4c6
AXEL JANIAK <axeljpro@gmail.com>
2024-04-08 11:33:15 +0200

    add style.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import './App.css'
 import Card from './components/Card'
+import './style.scss';
 
 function App() {
 
@@ -8,15 +8,15 @@ function App() {
       <section className="card">
         <img src="https://static.fnac-static.com/multimedia/Images/FR/NR/e3/36/b3/11744995/1540-1/tsp20200617101137/Adios-Bahamas.jpg" alt="nepal"></img>
         <section className='description'>
-        <h1>Nepal - Adios Bahamas</h1>
-        <h2>10 janvier 2020 - Rap</h2>
-        <section className='star'>
-        <img src='img/star_1828970.png'></img>
-        <img src='img/star_1828970.png'></img>
-        <img src='img/star_1828970.png'></img>
-        <img src='img/star_1828970.png'></img>
-        <img src='img/star_1828970.png'></img>
-        </section>
+          <h1>Nepal - Adios Bahamas</h1>
+          <h2>10 janvier 2020 - Rap</h2>
+          <section className='star'>
+            <img src='img/star_1828970.png'></img>
+            <img src='img/star_1828970.png'></img>
+            <img src='img/star_1828970.png'></img>
+            <img src='img/star_1828970.png'></img>
+            <img src='img/star_1828970.png'></img>
+          </section>
         </section>
       </section>
     </>
